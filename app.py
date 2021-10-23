592ed607571ca48be7c8d1676c85e1e02ace0442
Alex Hadjiev <36988416+daBawse167@users.noreply.github.com>
2021-10-23 15:49:03 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.run()
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/box', methods=['POST'])
+def box():
+    message = [x for x in request.form.values()][0]
+    return render_template("home.html", message=message)
